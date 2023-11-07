a7a517dc8726e633a0d1e2804d0dfc4705dd1c52
Bear <a.9876542@hotmail.com>
2023-11-07 14:39:41 +0800

    移除之前的語系檔案

--- lib/localization/qpp_locales.dart
+++ lib/localization/qpp_locales.dart
@@ -6,11 +6,13 @@ part 'qpp_locales.g.dart';
 @SheetLocalization(
   // Google Sheet URL 上擷取的 ID
   docId: '1ZLwcuLfsAr3VNAfdj9jya5diioRryaxIpLB0zfrKw9k',
-  // 代表多語系版本，每次更新 Google Sheet 後都可以更新此數字，接著重新生成檔案
+  // 代表多語系版本，每次更新 Google Sheet 後都可以更新此數字，接著 run build_runner 生成檔案
   version: 1,
   // 生成檔案的所在目錄
   outDir: 'assets/langs',
   // 生成檔案的名稱
   outName: 'langs.csv',
 )
+
+/// 多語系取用
 class _QppLocales {}
